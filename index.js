7d96b7d839480a52967e13837cff72220b049c96
satvik-tha-god <satviksinghx@gmail.com>
2021-08-07 02:19:59 +0530

    created models

--- index.js
+++ index.js
@@ -4,20 +4,34 @@ const {
 const gql = require('graphql-tag');
 const mongoose = require('mongoose'); //requiring mongoose for database
 
+const Post = require('./models/Post');
 const {
   MONGODB
 } = require('./config.js'); //requiring sensitive mongo info from config
 
 //setting up typedefs for app
 const typeDefs = gql`
+    type Post{
+      id: ID!
+      body: String!
+      createdAt: String!
+      username: String!
+    }
   type Query{
-    sayHi: String!
+    getPosts: [Post]
   }
-`
+`;
 //setting up our resolvers
 const resolvers = {
   Query: {
-    sayHi: () => 'Hello Satvik'
+    async getPosts(){
+      try{
+        const posts = await Post.find();
+        return posts;
+      } catch(err){
+        throw new Error(err);
+      }
+    }
   }
 }
 //setting up  apollo server
