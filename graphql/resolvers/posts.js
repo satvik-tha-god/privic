bf41e38413655b14555a09cfa5dadf20ec2d8fb3
satvik-tha-god <satviksinghx@gmail.com>
2021-08-09 01:36:58 +0530

    removed subscriptions

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -49,10 +49,6 @@ module.exports = {
       });
       const post = await newPost.save();
 
-      context.pubsub.publish('NEW_POST', {
-        newPost: post
-      })
-
       return post;
     },
     async deletePost(_, {
@@ -94,13 +90,6 @@ module.exports = {
         await post.save();
         return post;
       } else throw new UserInputError('Post not found')
-    }
-  },
-  Subscription: {
-    newPost: {
-      subscribe: (_, __, {
-        pubsub
-      }) => pubsub.asyncIterator('NEW_POST')
     }
   }
 };
