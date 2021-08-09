915063a1944f0eaaf51a1d0ff40d2330db01a8d4
satvik-tha-god <satviksinghx@gmail.com>
2021-08-09 12:23:33 +0530

    added Home Login Register pages

---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { Menu } from 'semantic-ui-react'
+
+function MenuBar() {
+
+  const [activeItem, setActiveItem] = useState('');
+
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+
+    return (
+        <Menu pointing secondary>
+          <Menu.Item
+            name='home'
+            active={activeItem === 'home'}
+            onClick={handleItemClick}
+          />
+          <Menu.Item
+            name='messages'
+            active={activeItem === 'messages'}
+            onClick={handleItemClick}
+          />
+
+          <Menu.Menu position='right'>
+            <Menu.Item
+              name='login'
+              active={activeItem === 'friends'}
+              onClick={handleItemClick}
+            />
+            <Menu.Item
+              name='register'
+              active={activeItem === 'logout'}
+              onClick={handleItemClick}
+            />
+          </Menu.Menu>
+        </Menu>
+    );
+}
+
+export default MenuBar;
