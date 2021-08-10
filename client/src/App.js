d3ba04afe82f68dae0f820cc3f250612f7fe5dea
satvik-tha-god <satviksinghx@gmail.com>
2021-08-10 13:19:55 +0530

    set up client again

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router,Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css'
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/login' component={Login}/>
+      <Route exact path='/register' component={Register}/>
+    </Router>
+  );
+}
+
+export default App;
