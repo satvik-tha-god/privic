38b032ed03dc4b0270806916e44e95751173479a
satvik-tha-god <satviksinghx@gmail.com>
2021-08-12 15:37:44 +0530

    minor

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -21,6 +21,7 @@ function PostCard({
           size="mini"
           src="https://react.semantic-ui.com/images/avatar/large/molly.png"
         />
+
         <Card.Header>{username}</Card.Header>
         <Card.Meta as={Link} to={`/posts/${id}`}>
           {moment(createdAt).fromNow(true)}
