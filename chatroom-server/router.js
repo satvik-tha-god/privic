0dcb5df4e3fd10e50e8a303ce66d9b1f6b242792
satvik-tha-god <satviksinghsw@gmail.com>
2022-04-05 21:15:05 +0530

    formatted: chatroom

--- chatroom-server/router.js
+++ chatroom-server/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/join", (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.send({ response: "Server is up and running." }).status(200);
 });
 
