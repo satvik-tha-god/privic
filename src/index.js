0ae2936f89fbb6d0bdef918753b5ee4efbe21f0c
satvik-tha-god <satviksinghsw@gmail.com>
2022-04-05 20:42:58 +0530

    tried making pwa

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 //App index
+import * as serviceWorker from './serviceWorker';
 import ReactDOM from "react-dom";
 import ApolloProvider from "./ApolloProvider";
 
 ReactDOM.render(ApolloProvider, document.getElementById("root"));
+
+serviceWorker.register();
